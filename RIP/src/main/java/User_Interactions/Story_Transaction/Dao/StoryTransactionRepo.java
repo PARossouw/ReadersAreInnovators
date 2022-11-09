54e50987225455c1a90ffd43be7e90190aacde1a
tarunsing <tsing53@gmail.com>
2022-11-09 21:17:26 +0200

    Implemented Story_transaction

--- RIP/src/main/java/User_Interactions/Story_Transaction/Dao/StoryTransactionRepo.java
+++ RIP/src/main/java/User_Interactions/Story_Transaction/Dao/StoryTransactionRepo.java
@@ -1,18 +1,18 @@
 package User_Interactions.Story_Transaction.Dao;
 
 import Story.Model.Story;
+import User.Model.Reader;
 import java.sql.SQLException;
-import user.Reader.Model.Reader;
 
-/**
- *
- * @author piete
- */
+
+
 public interface StoryTransactionRepo {
 
     boolean createEvent(Story story, Reader reader) throws SQLException;
     
     boolean updateEvent(Story story, Boolean bool, Reader reader) throws SQLException;
+    
+    
     
     
 
