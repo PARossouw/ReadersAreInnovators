6f8a918aef8715d043d88fea8362fae7aa479764
Pieter Rossouw <116373469+PARossouw@users.noreply.github.com>
2022-11-10 11:47:56 +0200

    Service Implementations

--- RIP/src/main/java/Story/Service/StoryService.java
+++ RIP/src/main/java/Story/Service/StoryService.java
@@ -2,41 +2,25 @@ package Story.Service;
 
 import Category.Model.Category;
 import Story.Model.Story;
-import User.Model.User;
 import User.Model.Writer;
 import java.util.List;
 
-
 public interface StoryService {
-    
+
     List<Story> searchStoriesByCategories(List<Category> categories);
-    
-    Boolean editPendingStoryByEditor(Story story);
-    
-    List<Story> viewWriterStories(Writer writer);
-    
-    Boolean saveDraftByWriter(Story story, Writer writer);
-    
-    Boolean submitCompletedAtory(Story story);
-    
-    Story viewSubmittedStory(Story story, Writer writer);
-    
+
+    List<Story> viewStroiesByWriter(Writer writer);
+
+    String saveStory(Story story);
+
+    String submitCompletedStory(Story story);
+
+    Story viewSubmittedStory(Story story);
+
     Story viewDraft(Story story);
-    
+
     List<Story> searchForStory(String storyParameter);
-    
+
     Story viewStoryDescription(Story story);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
 }
