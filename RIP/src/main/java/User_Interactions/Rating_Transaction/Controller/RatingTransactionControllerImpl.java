a320bc6aeba339ba2d9736ef7f3128f392222350
AntonMetrowich <ametrowich@gmail.com>
2022-12-04 23:00:48 +0200

    commit

--- RIP/src/main/java/User_Interactions/Rating_Transaction/Controller/RatingTransactionControllerImpl.java
+++ RIP/src/main/java/User_Interactions/Rating_Transaction/Controller/RatingTransactionControllerImpl.java
@@ -1,14 +1,13 @@
 package User_Interactions.Rating_Transaction.Controller;
 
 import Story.Dao.StoryRepoImpl;
-import Story.Model.Story;
-import User.Model.Reader;
 import User_Interactions.Rating_Transaction.Dao.RatingTransactionRepoImpl;
 import User_Interactions.Rating_Transaction.Service.RatingTransactionService;
 import User_Interactions.Rating_Transaction.Service.RatingTransactionServiceImpl;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -23,12 +22,13 @@ public class RatingTransactionControllerImpl {
         this.rating_transaction_service = new RatingTransactionServiceImpl(new RatingTransactionRepoImpl(), new StoryRepoImpl());
     }
 
-    @Path("/rateStory")
-    @POST
+    @Path("/rateStory/{ratingInfo}")
+    @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public void rateStory(JSONObject jsonObject) {
-        //return Response.status(Response.Status.OK).entity(rating_transaction_service.rateStory((Story)jsonObject.get("story"), (Reader)jsonObject.get("reader"), (Integer)jsonObject.get("rating"))).build();
+    public void rateStory(@PathParam("ratingInfo") String ratingInfo) {
+//        return Response.status(Response.Status.OK).entity(rating_transaction_service.rateStory(ratingInfo)).build();
+        rating_transaction_service.rateStory(ratingInfo);
     }
 
 }
